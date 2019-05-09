9bba70c968115262ecc6c4be8c4cd90bf968abdf
elfairyhyuk <elfairyhyuk@gmail.com>
2019-05-09 10:41:49 +0800

    test git

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using std::cout;
+using std::endl;
+
+int main(){
+    int *p=(int *)malloc(5);
+    p[0]=0;
+    p[1]=1;
+    p[2]=2;
+    p[3]=8;
+    p[4]=9;
+
+    int *ret=std::find(p,p+5,8);
+    cout<<"find "<<p[ret-p]<<"p address "<<ret-p<<"ret address "<<ret<<endl;
+    cout<<"find "<<*ret<<endl;
+    return 0;
+}
